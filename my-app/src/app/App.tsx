abe1cb569e6279f517827adabb4495effa7b88c2
Shizukesasss <crueljeffrey1904@gmail.com>
2026-02-09 11:00:35 +0800

    fix(App): Resolve conflicting routes in App.tsx after merge

--- my-app/src/app/App.tsx
+++ my-app/src/app/App.tsx
@@ -2,6 +2,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { HeroSection, GameModeSection } from '../pages/home';
 import { MonetaryMastery, QuizBee } from '../pages/games';
+import { WordHunt } from '../pages/games/WordHunt';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { SmoothScroll } from './components/SmoothScroll';
@@ -23,6 +24,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<MonetaryMastery />} />
+      <Route path="/word-hunt" element={<WordHunt />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/quiz-bee" element={<QuizBee />} />
     </Routes>
